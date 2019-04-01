9e740e8cd3df91dbf75470d24ef2e7e2d885e78e
Guillaume <46446766+GuillaumeLoup@users.noreply.github.com>
2019-04-01 17:44:36 +0200

    Update ex2.js

--- map/ex2.js
+++ map/ex2.js
@@ -43,7 +43,9 @@ En sortie: [
  */
 
 function getFoodCategories(foods) {
+  return foods.map(aliment => aliment.isVegetarian ? `${aliment.food} is suitable for vegetarians` : `${aliment.food} is not suitable for vegetarians`)
 }
+
 
 
 
