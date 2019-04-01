bc68a47dd87dfdf71f6a2f5f119b1e423f440b88
Guillaume <46446766+GuillaumeLoup@users.noreply.github.com>
2019-04-01 17:44:59 +0200

    Update ex3.js

--- map/ex3.js
+++ map/ex3.js
@@ -58,9 +58,19 @@ En sortie:
   ]
 
  */
-
 function getMoviesFreshness(movies) {
-}
+
+   return movies.map(film => {
+    if (film.rating < 60){
+      film.label = "rotten"
+    } else if (film.rating <= 75) {
+      film.label = "fresh"
+    } else {
+      film.label = "certified fresh"
+    }
+    return film
+})}
+
 
 
 
